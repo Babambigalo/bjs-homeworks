63775fabcd1a35c1972194a3822c87101fb6d3fa
Matveev Nikita <myatiy963@gmail.com>
2018-12-30 18:26:29 +0300

    lesson1

---
+++ variables/main.js
@@ -0,0 +1,12 @@
+//задача1
+let x = 2 , y = 22, z = 0;
+let result = x * y + 5 * z + x - 1;
+console.log(result);
+
+//задача2
+let algebra = 5, geography = 4, physics = 4;
+console.log((algebra + geography + physics)/3);
+
+//задача3
+let name = 'Никита';
+console.log(`Привет, мир! Меня зовут ${name}.`);
