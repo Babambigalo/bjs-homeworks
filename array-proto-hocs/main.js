01017455f21aa2fced05a64a7e5418a2b6680000
Matveev Nikita <myatiy963@gmail.com>
2019-02-09 00:55:53 +0300

    lesson 7

--- array-proto-hocs/main.js
+++ array-proto-hocs/main.js
@@ -1,19 +1,41 @@
+
+'use strict'
 //задача1
 function compareArrays(arr1,arr2) {
-    if(arr1.length === arr2.length) {
-        // arr1.forEach(item,i,arr1 => {
-        //     console.log( i + ": " + item + " (массив:" + arr1 + ")" );
-        //     // if(arr1[index] !== arr2[index]) return false;            
-        // });
-        arr1.forEach((item,i,arr1) => {
-            // console.log(item);
-            // console.log(arr1[i] !== arr2[i]);
-            if(arr1[i] !== arr2[i]) return false;
-            return true;
-        });
-        
+    if(arr1.length === arr2.length ) {
+        for(let i=0;i<arr1.length;i++) {
+            if(arr1[i] !== arr2[i]){
+                return false;
+            }
+        }
+        return true;
     }
-    //return false;
+    return false;    
 }
 
-console.log(compareArrays([1,2,4,5],[1,2,4,5]));
+//задача2
+function memoize(fn,limit) {
+    let results = [];
+    return function(a,b) {
+        for(let i=0;i<results.length;i++) {
+            if(compareArrays(results[i].args,[a,b]) === true) {
+                return results[i].result;        
+            }
+        }
+        results.push({
+            args: [a,b],
+            result: fn(a,b)
+        });
+
+        if(results.length > limit) results.shift(0);
+
+        return fn(a,b);
+    }
+}
+
+const sum = (a, b) => a + b;
+
+const mSum = memoize(sum, 2); 
+
+console.log(mSum( 3, 4 ));
+console.log(mSum( 3, 4 ));
